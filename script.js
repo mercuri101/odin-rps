f3611aa8c8d56ea6844fe9adcdbf8c2c29ef6522
mercuri <mercuri.abb@gmail.com>
2022-07-24 00:49:24 +0600

    Define playRound function

--- script.js
+++ script.js
@@ -2,12 +2,47 @@ function getComputerChoice() {
   let choice = Math.floor(Math.random()*3);
 
   if (choice == 0) {
-    return "Rock";
+    return "rock";
   }
   else if (choice == 1) {
-    return "Paper";
+    return "paper";
   }
   else {
-    return "Scissors";
+    return "scissors";
   }
 }
+
+function playRound(playerSelection, computerSelection) {
+
+  // If selections match, it's a draw
+  if (playerSelection.toLowerCase() === computerSelection) {
+    return "It's a draw!";
+  }
+
+  // Player plays rock
+  else if (playerSelection.toLowerCase() === "rock") {
+    return (computerSelection === "paper") ?
+      "You lose! Paper beats rock." :
+      "You win! Rock beats scissors";
+  }
+
+  // Player plays paper
+  else if (playerSelection.toLowerCase() === "paper") {
+    return (computerSelection === "scissors") ?
+      "You lose! Scissors beat paper." :
+      "You win! Paper beats rock.";
+  }
+
+  // Player plays scissors
+  else{
+    return (computerSelection === "rock") ?
+      "You lose! Rock beats scissors." :
+      "You win! Scissors beat paper.";
+  }
+}
+
+
+const playerSelection = "PAPER";
+const computerSelection = getComputerChoice();
+console.log("Computer chose " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
