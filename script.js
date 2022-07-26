492b5a6f18d0380b629a034f68e6715f14fb65a9
mercuri <mercuri.abb@gmail.com>
2022-07-26 17:13:00 +0600

    Implement the score limit logic and add restart button

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+const body = document.querySelector("body");
 const selections = document.querySelectorAll(".selection");
 const display = document.querySelector(".results");
+const scoredis = document.querySelector(".score");
+const score = document.querySelector(".cur-score");
+const selContainer = document.querySelector(".selections");
 
-// For each selection play round on click with the selection and 
+let playerScore = 0, computerScore = 0;
+
+// For each selection play round on click with the selection and
 // computer's choice as arguments.
 selections.forEach(sel => sel.addEventListener("click",
   () => playRound(sel.getAttribute("data-selection"), getComputerChoice())));
@@ -29,18 +35,17 @@ function playRound(playerSelection, computerSelection)
   // If selections match, it's a draw
   if (playerSelection.toLowerCase() === computerSelection) {
     display.textContent = "It's a draw!";
-    return 0;
   }
 
   // Player plays rock
   else if (playerSelection.toLowerCase() === "rock") {
     if (computerSelection === "paper") {
       display.textContent = "You lose! Paper beats rock.";
-      return -1;
+      computerScore++;
     }
     else {
       display.textContent = "You win! Rock beats scissors";
-      return 1;
+      playerScore++;
     }
   }
 
@@ -48,21 +53,50 @@ function playRound(playerSelection, computerSelection)
   else if (playerSelection.toLowerCase() === "paper") {
     if (computerSelection === "scissors") {
       display.textContent = "You lose! Scissors beat paper.";
-      return -1;
+      computerScore++;
     }
+    else {
       display.textContent = "You win! Paper beats rock.";
-      return 1;
+      playerScore++;
+    }
   }
 
   // Player plays scissors
   else{
     if (computerSelection === "rock") {
       display.textContent = "You lose! Rock beats scissors.";
-      return -1;
+      computerScore++;
     }
+    else {
       display.textContent = "You win! Scissors beat paper.";
-      return 1;
+      playerScore++;
+    }
   }
+
+  // After each round, show score and check for the victor
+  score.textContent = `${playerScore} - ${computerScore}`;
+  if (playerScore == 5 || computerScore == 5) {
+    display.textContent = (playerScore == 5) ? "YOU WIN" : "COMPUTER WINS";
+    display.setAttribute("class", "final-result");
+    promptRestart();
+  }
+}
+
+function promptRestart() {
+  const restartBtn = document.createElement("button");
+  restartBtn.textContent = "Restart";
+  body.insertBefore(restartBtn, selContainer);
+  selContainer.remove();
+
+  restartBtn.addEventListener("click", () => {
+    score.textContent = "0 - 0";
+    display.textContent = "";
+    display.setAttribute("class", "results");
+    playerScore = 0;
+    computerScore = 0;
+    restartBtn.remove();
+    body.insertBefore(selContainer, display);
+  })
 }
 
 function reportGameResult(playerScore, computerScore)
